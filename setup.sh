38b38b70b51f518ce5d3ea4b32bd03f9df4a1f77
jimye0702 <br19920702@gmail.com>
2021-06-28 17:01:22 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"br19920702@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
